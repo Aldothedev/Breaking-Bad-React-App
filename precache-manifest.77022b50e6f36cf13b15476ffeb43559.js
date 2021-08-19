21cd99874bbe1bf12e6c2ff934b2bede0cb18441
Miguels <adolfolaguna007@gmail.com>
2021-08-19 16:25:55 -0400

    Updates

---
+++ precache-manifest.77022b50e6f36cf13b15476ffeb43559.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027a12de8ee7b051cf804dde0c04401f",
+    "url": "/Breaking-Bad-React-App/index.html"
+  },
+  {
+    "revision": "7463988c757cdd8fd2fa",
+    "url": "/Breaking-Bad-React-App/static/css/main.3673bbfc.chunk.css"
+  },
+  {
+    "revision": "fa627846aa1b24f26d7c",
+    "url": "/Breaking-Bad-React-App/static/js/2.74a3b4d3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Breaking-Bad-React-App/static/js/2.74a3b4d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7463988c757cdd8fd2fa",
+    "url": "/Breaking-Bad-React-App/static/js/main.7ef3cdcd.chunk.js"
+  },
+  {
+    "revision": "113f199a16a1cea0e418",
+    "url": "/Breaking-Bad-React-App/static/js/runtime-main.9843d6e4.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking-Bad-React-App/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Breaking-Bad-React-App/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Breaking-Bad-React-App/static/media/spinner.235b9b3e.gif"
+  }
+]);
